5c67991844911bbbdf8af0259e7c22fa88d9fe5f
Vin <121345613+wh0ois@users.noreply.github.com>
2023-06-28 21:18:37 +0530

    Update pkg_mgmt.py
    check dependencies of the specified package and ensure that all necessary dependencies are installed

--- pkg_mgmt.py
+++ pkg_mgmt.py
@@ -17,12 +17,30 @@ def search_packages(query):
 
 # Install
 def install_package(package_name):
-    try:
-        subprocess.run(['sudo', 'dnf', 'install', '-y', package_name], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred while installing the package: {e}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+    # Execute dnf install command with the package name and the --assumeyes flag for non-interactive installation
+    install_command = ['sudo', 'dnf', 'install', '-y', package_name]
+    subprocess.run(install_command)
+
+    # Check if the installation was successful
+    if subprocess.run(['sudo', 'dnf', 'list', package_name], capture_output=True).returncode == 0:
+        print(f"Package '{package_name}' installed successfully.")
+
+        # Resolve package dependencies
+        resolve_command = ['sudo', 'dnf', 'repoquery', '--requires', package_name]
+        output = subprocess.check_output(resolve_command).decode('utf-8')
+
+        # Split the output into a list of dependency package names
+        dependency_packages = output.strip().split('\n')
+
+        # Install the required dependency packages
+        if dependency_packages:
+            print("Installing package dependencies...")
+            install_command = ['sudo', 'dnf', 'install', '-y'] + dependency_packages
+            subprocess.run(install_command)
+            print("Package dependencies installed successfully.")
+    else:
+        print(f"Failed to install package '{package_name}'.")
+
 
 
 # Update
