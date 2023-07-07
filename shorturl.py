6b8c6741a118ef77e8681b176d3ef6ac49e9da49
Vin <121345613+wh0ois@users.noreply.github.com>
2023-07-07 20:28:27 +0530

    Update shorturl.py

--- shorturl.py
+++ shorturl.py
@@ -1,6 +1,6 @@
 import json
 import time
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 from pymemcache.client import base as memcache
@@ -9,6 +9,7 @@ from botocore.exceptions import ClientError
 import hashlib
 import logging
 import redis
+from multiprocessing import Pool
 
 app = Flask(__name__)
 kafka_bootstrap_servers = 'localhost:9092'
@@ -18,7 +19,6 @@ memcache_port = 11211
 redis_host = 'localhost'
 redis_port = 6379
 redis_ttl = 7 * 24 * 60 * 60  # 7 days in seconds
-dynamodb_batch_size = 25  # Batch size for DynamoDB write operations
 
 producer = KafkaProducer(bootstrap_servers=kafka_bootstrap_servers)
 memcache_client = memcache.Client((memcache_host, memcache_port))
@@ -28,6 +28,25 @@ redis_client = redis.Redis(host=redis_host, port=redis_port)
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+def write_batch_to_dynamodb(batch_items):
+    with dynamodb_client.batch_write_item(
+            RequestItems={
+                dynamodb_table_name: batch_items
+            }
+    ):
+        pass
+
+def fetch_long_urls(short_ids):
+    response = dynamodb_client.batch_get_item(
+        RequestItems={
+            dynamodb_table_name: {
+                'Keys': [{'id': {'S': short_id}} for short_id in short_ids],
+                'ProjectionExpression': 'id, long_url'
+            }
+        }
+    )
+    return {item['id']['S']: item['long_url']['S'] for item in response['Responses'][dynamodb_table_name]}
 
 @app.route('/shorten', methods=['POST'])
 def shorten_url():
@@ -48,32 +67,6 @@ def shorten_url():
                 'long_url': long_url
             }
             send_kafka_message(kafka_message)
-
-            # Batch write operations for DynamoDB
-            batch_items = []
-            batch_items.append({
-                'Put': {
-                    'TableName': dynamodb_table_name,
-                    'Item': {
-                        'id': {'S': unique_id},
-                        'short_url': {'S': short_url},
-                        'long_url': {'S': long_url}
-                    }
-                }
-            })
-            batch_items.append({
-                'Put': {
-                    'TableName': dynamodb_table_name,
-                    'Item': {
-                        'id': {'S': unique_id},
-                        'long_url': {'S': long_url}
-                    },
-                    'ConditionExpression': 'attribute_not_exists(id)'
-                }
-            })
-
-            # Perform batch write operations
-            write_batches(batch_items, dynamodb_batch_size)
 
             # Store data in Memcache
             memcache_client.set(short_url, long_url, time=redis_ttl)
@@ -105,18 +98,6 @@ def send_kafka_message(message):
 
     raise Exception('Failed to send Kafka message')
 
-def write_batches(items, batch_size):
-    """
-    Write items to DynamoDB in batches.
-    """
-    for i in range(0, len(items), batch_size):
-        batch_items = items[i:i+batch_size]
-        try:
-            dynamodb_client.transact_write_items(TransactItems=batch_items)
-        except ClientError as e:
-            logger.error(f"DynamoDB error occurred during batch write: {str(e)}")
-            raise
-
 @app.route('/<short_id>')
 def redirect_url(short_id):
     # Check if the long URL is in cache
@@ -124,14 +105,20 @@ def redirect_url(short_id):
 
     if not long_url:
         try:
-            response = dynamodb_client.get_item(
-                TableName=dynamodb_table_name,
-                Key={'id': {'S': short_id}},
-                ProjectionExpression='long_url'
-            )
-            item = response.get('Item')
-            if item:
-                long_url = item.get('long_url', {}).get('S')
+            # Fetch the long URLs in parallel
+            with Pool() as pool:
+                long_urls = pool.map(fetch_long_urls, [short_id])[0]
+
+            long_url = long_urls.get(short_id)
+
+            if long_url:
+                # Increment the redirection count in DynamoDB
+                dynamodb_client.update_item(
+                    TableName=dynamodb_table_name,
+                    Key={'id': {'S': short_id}},
+                    UpdateExpression='SET redirection_count = redirection_count + :val',
+                    ExpressionAttributeValues={':val': {'N': '1'}}
+                )
 
                 # Cache the long URL
                 redis_client.set(short_id, long_url, ex=redis_ttl)
